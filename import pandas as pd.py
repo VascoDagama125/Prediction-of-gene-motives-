48e0bd2a9edb1a60cb67d6dbd58c0d07c66db4e1
Pawel <paul.dyngosz@gmail.com>
2024-11-18 11:36:11 +0100

    program from 18.11.2024

---
+++ import pandas as pd.py
@@ -0,0 +1,10 @@
+import pandas as pd
+#excel_file = "arabidopsis_thaliana_tair_gtf.xlsx"
+#df = pd.read_excel(excel_file, sheet_name=1)
+
+excel_file = "arabidopsis_thaliana_tair_gtf.xlsx"
+df = pd.read_excel(excel_file)
+print(df)
+
+
+
